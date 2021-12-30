bc23121bba68c931b409d088c2617e9052b65429
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-12-29 21:40:43 -0800

    index.js: play function: created

--- index.js
+++ index.js
@@ -20,5 +20,51 @@ var vizInit = function (){
     audio.play();
     play();
   }
+
+  function play() {
+    var context = new AudioContext();
+    var src = context.createMediaElementSource(audio);
+    var analyser = context.createAnalyser();
+    src.connect(analyser);
+    analyser.connect(context.destination);
+    analyser.fftSize = 512;
+    var bufferLength = analyser.frequencyBinCount;
+    var dataArray = new Uint8Array(bufferLength);
+    var scene = new THREE.Scene();
+    var group = new THREE.Group();
+    var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0,0,100);
+    camera.lookAt(scene.position);
+    scene.add(camera);
+
+    var renderer = new THREE.WebGLRenderer({ alpha: true, antialias: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    var planeGeometry = new THREE.PlaneGeometry(800, 800, 20, 20);
+    var planeMaterial = new THREE.MeshLambertMaterial({
+        color: 0x6904ce,
+        side: THREE.DoubleSide,
+        wireframe: true
+    });
+    
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane.rotation.x = -0.5 * Math.PI;
+    plane.position.set(0, 30, 0);
+    group.add(plane);
+    
+    var plane2 = new THREE.Mesh(planeGeometry, planeMaterial);
+    plane2.rotation.x = -0.5 * Math.PI;
+    plane2.position.set(0, -30, 0);
+    group.add(plane2);
+
+    var icosahedronGeometry = new THREE.IcosahedronGeometry(10, 4);
+    var lambertMaterial = new THREE.MeshLambertMaterial({
+        color: 0xff00ee,
+        wireframe: true
+    });
+
+    var ball = new THREE.Mesh(icosahedronGeometry, lambertMaterial);
+    ball.position.set(0, 0, 0);
   
 } 
+}
