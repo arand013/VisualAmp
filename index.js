04f8f4ea68791bc60e0c4670de47dad9a7c4c9bf
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-12-29 21:35:19 -0800

    index.js: created

---
+++ index.js
@@ -0,0 +1,24 @@
+var noise = new SimplexNoise();
+var vizInit = function (){
+  
+  var file = document.getElementById("thefile");
+  var audio = document.getElementById("audio");
+  var fileLabel = document.querySelector("label.file");
+  
+  document.onload = function(e){
+    console.log(e);
+    audio.play();
+    play();
+  }
+  file.onchange = function(){
+    fileLabel.classList.add('normal');
+    audio.classList.add('active');
+    var files = this.files;
+    
+    audio.src = URL.createObjectURL(files[0]);
+    audio.load();
+    audio.play();
+    play();
+  }
+  
+} 
