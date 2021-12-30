2eeb0c3961ec013169cecf13b3ecb6ee60ac074d
Alexander Aranda <Alexandermarkets1@gmail.com>
2021-12-29 21:43:49 -0800

    index.js: makeRouchBall function: updated

--- index.js
+++ index.js
@@ -156,5 +156,24 @@ document.body.addEventListener('touchend', function(ev) { context.resume(); });
 
 
 
+function fractionate(val, minVal, maxVal) {
+    return (val - minVal)/(maxVal - minVal);
+}
+
+function modulate(val, minVal, maxVal, outMin, outMax) {
+    var fr = fractionate(val, minVal, maxVal);
+    var delta = outMax - outMin;
+    return outMin + (fr * delta);
+}
+
+function avg(arr){
+    var total = arr.reduce(function(sum, b) { return sum + b; });
+    return (total / arr.length);
+}
+
+function max(arr){
+    return arr.reduce(function(a, b){ return Math.max(a, b); })
+}
+
 
 
